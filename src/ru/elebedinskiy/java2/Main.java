8e062f84550f843e77e84c30480035c893a43b52
Evgeniy Lebedinskiy <el@lecorp.ru>
2020-06-13 21:28:50 +0300

    объявил и присвоил два массива

--- src/ru/elebedinskiy/java2/Main.java
+++ src/ru/elebedinskiy/java2/Main.java
@@ -1,8 +1,30 @@
 package ru.elebedinskiy.java2;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	    // write your code here
+        String[][] array1 = {
+                {"18","#1","18","54"},
+                {"28","45","90","54"},
+                {"45","3","symbol","54"},
+                {"text","3","14","54"}
+        };
+        String[][] array2 = {
+                {"18","#1","18"},
+                {"28","45","90","54"},
+                {"28","45","90","54","test"},
+                {"45","3","symbol","54"},
+                {"text","3","14","54"}
+        };
+
+        System.out.println("Исходный массив #1: " + Arrays.deepToString(array1));
+        System.out.println("Исходный массив #2: " + Arrays.deepToString(array2));
+
+        System.out.println("\nПервый массив");
+        //procArray(array1,4);
+        System.out.println("\nВторой массив");
+        //procArray(array2,4);
     }
 }
