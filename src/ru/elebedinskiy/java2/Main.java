6e5825834f9a8122fb92df9549abbda20a6d7afc
Evgeniy Lebedinskiy <el@lecorp.ru>
2020-06-14 19:42:29 +0300

    добавил комментарии и объявил размерность массива отдельной переменной

--- src/ru/elebedinskiy/java2/Main.java
+++ src/ru/elebedinskiy/java2/Main.java
@@ -5,12 +5,14 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
+        // массив размером 4 на 4
         String[][] array1 = {
                 {"18","#1","18","54"},
                 {"28","45","90","54"},
                 {"45","3","symbol","54"},
                 {"text","3","14","54"}
         };
+        // массив размером 5 на 4
         String[][] array2 = {
                 {"18","#1","18"},
                 {"28","45","90"},
@@ -18,26 +20,33 @@ public class Main {
                 {"45","3","symbol"},
                 {"text","3","14"}
         };
+        // массив размеров 4 на 5
         String[][] array3 = {
                 {"18","#1","18","hi","33"},
                 {"28","45","90","54","33"},
                 {"28","45","90","54","test"},
                 {"45","3","symbol","54","33"},
-                {"text","3","14","54","33"}
         };
 
         System.out.println("Исходный массив #1: " + Arrays.deepToString(array1));
         System.out.println("Исходный массив #2: " + Arrays.deepToString(array2));
+        System.out.println("Исходный массив #3: " + Arrays.deepToString(array3));
+
+        int size = 4; // размерность двумерного массива, с которой будем работать
 
         System.out.println("\nРаботаем с первым массивом");
-        procArray(array1,4);
+        procArray(array1,size);
         System.out.println("\nРаботаем со вторым массивом");
-        procArray(array2,4);
+        procArray(array2,size);
         System.out.println("\nРаботаем с третим массивом");
-        procArray(array3,4);
+        procArray(array3,size);
+
+        System.out.println("\nОбработанный массив #1: " + Arrays.deepToString(array1));
+        System.out.println("Обработанный массив #2: " + Arrays.deepToString(array2));
+        System.out.println("Обработанный массив #3: " + Arrays.deepToString(array3));
     }
 
-    // метод валидации массива
+    // метод валидации двумерного массива
     public static boolean validArray(String[][] array, int size) {
         String[][] validArray = new String[size][size];
         try {
@@ -54,7 +63,7 @@ public class Main {
         return true;
     }
 
-    // метод обработки массива
+    // метод обработки двумерного массива
     public static void procArray(String[][] array, int size) {
         if (validArray(array, size) == false){
             System.out.println("Ошибка! Массив не соответствует размеру " + size + " х " + size + "!");
