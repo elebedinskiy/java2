f7adf04bfb3cb3b447bd00c05262b5f04dab1265
Evgeniy Lebedinskiy <el@lecorp.ru>
2020-06-15 22:21:58 +0300

    разработки завершена

--- src/ru/elebedinskiy/java2/Main.java
+++ src/ru/elebedinskiy/java2/Main.java
@@ -1,6 +1,5 @@
 package ru.elebedinskiy.java2;
 
-import javax.naming.spi.ObjectFactoryBuilder;
 import java.util.Arrays;
 
 public class Main {
@@ -28,34 +27,43 @@ public class Main {
                 {"28","45","90","54","test"},
                 {"45","3","symbol","54","33"},
         };
+        // массив размеров 3 на 3
+        String[][] array4 = {
+                {"18","#1","33"},
+                {"28","45","33"},
+                {"28","45","test"},
+        };
 
         System.out.println("Исходный массив #1: " + Arrays.deepToString(array1));
         System.out.println("Исходный массив #2: " + Arrays.deepToString(array2));
         System.out.println("Исходный массив #3: " + Arrays.deepToString(array3));
+        System.out.println("Исходный массив #4: " + Arrays.deepToString(array4));
 
         int size = 4; // размерность двумерного массива, с которой будем работать
 
-        System.out.println("\nРаботаем с первым массивом");
+        System.out.println("\nПробуем работать с массивов #1, размером 4 х 4:");
         procArray(array1,size);
-        System.out.println("\nРаботаем со вторым массивом");
+        System.out.println("\nПробуем работать с массивов #2, размером 5 х 4:");
         procArray(array2,size);
-        System.out.println("\nРаботаем с третьим массивом");
+        System.out.println("\nПробуем работать с массивов #3, размером 4 х 5:");
         procArray(array3,size);
+        System.out.println("\nПробуем работать с массивов #4, размером 3 х 3:");
+        procArray(array4,size);
 
         // для самопроверки
         System.out.println("\nОбработанный массив #1: " + Arrays.deepToString(array1));
         System.out.println("Обработанный массив #2: " + Arrays.deepToString(array2));
         System.out.println("Обработанный массив #3: " + Arrays.deepToString(array3));
-
+        System.out.println("Обработанный массив #4: " + Arrays.deepToString(array4));
     }
 
     // метод валидации двумерного массива
-    public static boolean MyArraySizeException(String[][] array, int size) {
+    public static boolean validArray(String[][] array, int size) {
         String[][] validArray = new String[size][size];
         try {
             for (int i = 0; i < size; i++) {
                 if (array.length != size || array[i].length != size) { // размер столбцов не равен size и размер конкретной строки не равен size
-                    for (int j = size - 1; j <= size; j++) { // попробуем обратиться к элементу до и после size
+                    for (int j = size - 1; j <= size; j++) { // попробуем обратиться к элементу строки до и после size
                         validArray[i][j] = array[i][j];
                     }
                 }
@@ -68,25 +76,16 @@ public class Main {
 
     // метод обработки двумерного массива
     public static void procArray(String[][] array, int size) {
-        if (MyArraySizeException(array, size) == false){
+        if (validArray(array, size) == false){
             System.out.println("Ошибка! Массив не соответствует размеру " + size + " х " + size + "!");
-        } else if (MyArraySizeException(array, size) == true){
+        } else if (validArray(array, size) == true){
             System.out.println("Массив размера " + size + " х " + size + ", работаем дальше...");
-            System.out.println("Обработанный массив: " + Arrays.deepToString(MyArrayDataException(array)));
+            System.out.println("Обработанный массив: " + Arrays.deepToString(sumArray(array)));
         }
-    }
-
-    private static int[][] sumProcArray(int[][] array) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                array[i][j]++;
-            }
-        }
-        return array;
     }
 
     // метод работы с целочисленными значениями элементов двумерного массива
-    public static String[][] MyArrayDataException (String[][] array){
+    public static String[][] sumArray(String[][] array){
         for (int i = 0; i < array.length; i++){
             for (int j = 0; j < array[i].length; j++){
                 try {
