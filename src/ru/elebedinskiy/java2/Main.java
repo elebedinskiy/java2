13aab4c73b1200d490683bfbf9215ce057fba1f0
Evgeniy Lebedinskiy <el@lecorp.ru>
2020-06-27 09:36:28 +0300

    done

--- src/ru/elebedinskiy/java2/Main.java
+++ src/ru/elebedinskiy/java2/Main.java
@@ -1,49 +1,60 @@
 package ru.elebedinskiy.java2;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
 public class Main {
 
+    static final int SIZE = 10_000_000;
+    static final int HALF = SIZE / 2;
+
     public static void main(String[] args) {
-        final int SIZE = 10_000_000;
-        final int HALF = SIZE / 2;
-        float[] arr1 = new float[SIZE];
-        float[] arr2 = new float[SIZE];
-        float[] arr21 = new float[HALF];
-        float[] arr22 = new float[HALF];
 
-        // ЭТО БУДЕТ МЕТОД 1
-        // заполним оба массива единицами
-        for (int i = 0; i < arr1.length; i++){
-            arr1[i] = 1;
-            arr2[i] = 1;
-        }
+        float[] array1 = new float[SIZE];
+        float[] array2 = new float[SIZE];
 
-        // ЭТО БУДЕТ МЕТОД 2
-        // проведем вычисления с замером времени в одном потоке
-        long start1 = System.currentTimeMillis();
-        for (int i = 0; i < arr1.length; i++){
-            arr1[i] = (float)(arr1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-        }
-        long end1 = System.currentTimeMillis();
-        System.out.println("time 1: " + (end1 - start1));
-        // end
+        procArray0to1(array1);
+        procArray0to1(array2);
 
-
-        // ЭТО БУДЕТ МЕТОД 3
-        long start2 = System.currentTimeMillis();
-        // разделим массива на два массива одинаковой длинны
-        System.arraycopy(arr2, 0, arr21, 0, HALF);
-        System.arraycopy(arr2, HALF, arr22, 0, HALF);
-
-        // проведём исчисления в разных потоках;
-
-        // склеим массив обратно в один целый
-        System.arraycopy(arr21, 0, arr2, 0, HALF);
-        System.arraycopy(arr22, 0, arr2, HALF, HALF);
-        long end2 = System.currentTimeMillis();
-        System.out.println("time 2: " + (end2 - start2));
+        procArray1thread(array1);
+        procArray2thread(array2);
 
     }
+
+    public static void procArray0to1 (float[] array){
+        for (int i = 0; i < array.length; i++){
+            array[i] = 1;
+        }
+    }
+
+    public static void procArray1thread (float[] array){
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < array.length; i++){
+            array[i] = (float)(array[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("time 1: " + (end - start));
+    }
+
+    public static void procArray2thread (float[] array){
+
+        float[] arr1 = new float[HALF];
+        float[] arr2 = new float[HALF];
+
+        long start = System.currentTimeMillis();
+
+        // разделим массив на два массива одинаковой длинны
+        System.arraycopy(array, 0, arr1, 0, HALF);
+        System.arraycopy(array, HALF, arr2, 0, HALF);
+
+        // проведем вычисления в двух разных потоках
+        new Thread(() -> new NewThread().procArrayInNewThread(arr1)).start();
+        new Thread(() -> new NewThread().procArrayInNewThread(arr2)).start();
+
+        // склеим массив обратно в один целый
+        System.arraycopy(arr1, 0, array, 0, HALF);
+        System.arraycopy(arr2, 0, array, HALF, HALF);
+
+        long end = System.currentTimeMillis();
+        System.out.println("time 2: " + (end - start));
+
+    }
+
 }
---
+++ src/ru/elebedinskiy/java2/NewThread.java
@@ -0,0 +1,16 @@
+package ru.elebedinskiy.java2;
+
+public class NewThread extends Thread {
+
+    @Override
+    public void run(){
+
+    }
+
+    public void procArrayInNewThread (float[] array) {
+        for (int i = 0; i < array.length; i++){
+            array[i] = (float)(array[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+    }
+
+}
