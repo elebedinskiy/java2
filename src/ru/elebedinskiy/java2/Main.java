7c175beda7e502cdcdbb2e5653d9e7db07fdd57d
Evgeniy Lebedinskiy <el@lecorp.ru>
2020-06-28 11:00:04 +0300

    ref

--- src/ru/elebedinskiy/java2/Main.java
+++ src/ru/elebedinskiy/java2/Main.java
@@ -7,8 +7,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //System.out.println("Thread name: " + Thread.currentThread().getName()); // для проверки
-
         float[] array1 = new float[SIZE];
         float[] array2 = new float[SIZE];
 
@@ -26,13 +24,19 @@ public class Main {
         }
     }
 
-    public static void procArray1thread (float[] array){
-        long start = System.currentTimeMillis();
+
+    public static void calcArray (float[] array){
         for (int i = 0; i < array.length; i++){
+            //System.out.println("Thread name: " + Thread.currentThread().getName()); // для проверки
             array[i] = (float)(array[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
-        long end = System.currentTimeMillis();
-        System.out.println("time 1: " + (end - start));
+        //System.out.println("Обработка завершена"); // для проверки
+    }
+
+    public static void procArray1thread (float[] array){
+        long s = System.currentTimeMillis();
+        calcArray(array);
+        System.out.println("time 1: " + (System.currentTimeMillis() - s));
     }
 
     public static void procArray2thread (float[] array){
@@ -40,22 +44,21 @@ public class Main {
         float[] arr1 = new float[HALF];
         float[] arr2 = new float[HALF];
 
-        long start = System.currentTimeMillis();
+        long s = System.currentTimeMillis();
 
         // разделим массив на два массива одинаковой длинны
         System.arraycopy(array, 0, arr1, 0, HALF);
         System.arraycopy(array, HALF, arr2, 0, HALF);
 
         // проведем вычисления в двух разных потоках
-        new Thread(() -> new NewThread().procArrayInNewThread(arr1)).start();
-        new Thread(() -> new NewThread().procArrayInNewThread(arr2)).start();
+        new Thread(() -> new Main().calcArray(arr1)).start();
+        new Thread(() -> new Main().calcArray(arr2)).start();
 
         // склеим массив обратно в один целый
         System.arraycopy(arr1, 0, array, 0, HALF);
         System.arraycopy(arr2, 0, array, HALF, HALF);
 
-        long end = System.currentTimeMillis();
-        System.out.println("time 2: " + (end - start));
+        System.out.println("time 2: " + (System.currentTimeMillis() - s));
 
     }
 
